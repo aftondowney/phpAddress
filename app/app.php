066369388b19b8401b104d1db6564e11fdf4539b
Afton Downey <aaike001@gmail.com>
2016-02-12 09:14:52 -0800

    add list template

--- app/app.php
+++ app/app.php
@@ -4,11 +4,20 @@
 
     $app = new Silex\Application();
 
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+		));
+
+
     session_start();
-    
+
 		if (empty($_SESSION['list_of_contacts'])) {
 		    $_SESSION['list_of_contacts'] = array();
 		}
+
+		$app->get("/", function() use ($app) {
+				return $app['twig']->render('list.html.twig');
+		});
 
 
     return $app;
