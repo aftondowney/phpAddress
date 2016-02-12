68b0497ff5b186fb05597f050e989c44491f4c67
Afton Downey <aaike001@gmail.com>
2016-02-12 10:24:38 -0800

    add delete_contacts functionality

--- app/app.php
+++ app/app.php
@@ -23,7 +23,12 @@
     $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);            
     $contact->save();
     return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
-});
+		});
+
+		$app->post("/delete_contacts", function() use ($app) {
+    Contact::deleteAll();
+    return $app['twig']->render('delete_contacts.html.twig');
+		});
 
 
     return $app;
